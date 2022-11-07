1cb03e99fbed3baf9cd5409680f75dbc3c8a1839
Ocean <77823676+Ocean48@users.noreply.github.com>
2022-11-07 18:41:19 -0500

    Product page update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
-// 
+fetch("../data/data.json")
+    .then((res) => {
+        return res.json();
+    })
+    .then((data) => {
+        // For each key in object data
+        Object.keys(data).forEach(key => {
+            // data[key].length -> length of value of a key (which is array)
+            var gallery = document.getElementById("gallery");  // Get gallery div
+            var title = document.createElement("h2");
+            var hr = document.createElement("hr");
+            title.innerHTML = (key);
+            gallery.appendChild(title);
+            
+                
+
+            for (var i = 0; i < data[key].length; i++) {
+                var block = document.createElement("div");  // Greate div for each item 
+                var p = document.createElement('p');  // Create p for info in each item
+                p.innerHTML = (data[key][i].name + "  P:" + data[key][i].price);
+
+
+                var img = document.createElement("img");  // Create img tag
+                img.src = ("../images/" + data[key][i].image);
+                img.alt = ("Image of " + data[key][i].name);
+
+                gallery.appendChild(block);
+                block.appendChild(img);
+                block.appendChild(p);
+
+                console.log(data[key][i].name + "  " + data[key][i].image + "  " + data[key][i].price);
+            };
+
+            gallery.appendChild(hr);
+        });
+    });
